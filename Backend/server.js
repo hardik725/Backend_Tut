6a72ee94b8d12362b4dd60452ff57ed8ff7e8d26
Hardik <hardik.pandey.716@gmail.com>
2025-03-29 21:14:26 +0530

    second

--- Backend/server.js
+++ Backend/server.js
@@ -1,47 +1,25 @@
-// const express = require('expresss');
 import express from 'express';
 
 const app = express();
 
-// app.get('/', (req, res) =>{
-//     res.send('server is ready');
-// });
+// Get a list of 5 jokes
+app.get('/', (req,res) => {
+    res.send('Server is Running');
+})
 
-// get a list of a 5 jokes
-
-app.get('/jokes', (req, res) =>{
-const jokes = [
-    {
-        id: 1,
-        title: '1 joke',
-        content: 'This is a 1st joke'
-    },
-    {
-        id: 1,
-        title: '2 joke',
-        content: 'This is a 2nd joke'
-    },
-    {
-        id: 1,
-        title: '3 joke',
-        content: 'This is a 3rd joke'
-    },
-    {
-        id: 1,
-        title: '4 joke',
-        content: 'This is a 4th joke'
-    },
-    {
-        id: 1,
-        title: '5 joke',
-        content: 'This is a 5th joke'
-    }
-];
-res.send(jokes);
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        { id: 1, title: '1st joke', content: 'This is the 1st joke' },
+        { id: 2, title: '2nd joke', content: 'This is the 2nd joke' },
+        { id: 3, title: '3rd joke', content: 'This is the 3rd joke' },
+        { id: 4, title: '4th joke', content: 'This is the 4th joke' },
+        { id: 5, title: '5th joke', content: 'This is the 5th joke' }
+    ];
+    res.json(jokes); // Use `res.json()` instead of `res.send()`
 });
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, ()=>{
-    console.log(`Server at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
